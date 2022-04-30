d66535a1817879ea05c9266f395382c2e3cf3c3a
LuckDuracell <lukedru@yahoo.com>
2022-04-30 12:18:41 -0700

    Initial Commit

---
+++ Calico/CalicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalicoApp.swift
+//  Calico
+//
+//  Created by Luke Drushell on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CalicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
